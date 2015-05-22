6a12d431417aed50e5ca6e05b235962c57cb1ded
andre godfrey <andrecodes31@gmail.com>
2015-05-22 07:46:28 -0400

    week3:download,install,activate and configure the wordpress popular posts plugin

--- wordpress/wp-content/themes/skillcrushstarter/functions.php
+++ wordpress/wp-content/themes/skillcrushstarter/functions.php
@@ -77,3 +77,38 @@ function skillcrush_comments($comment, $args, $depth) {
 }
 
 
+
+
+
+
+
+
+
+// changes excerpt symbol
+function custom_excerpt_more($more) {
+	return '...';
+}
+add_filter('excerpt_more', 'custom_excerpt_more');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
